8e5f6f1927ae55a49569fe664840ad5280a78ea8
pdejonghe <dejonghe.peter@gmail.com>
2018-12-06 09:28:48 +0100

    Adding Swagger

--- SwaggerWebAPI/Controllers/ValuesController.cs
+++ SwaggerWebAPI/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SwaggerWebAPI.Models;
 
 namespace SwaggerWebAPI.Controllers
 {
@@ -10,18 +11,45 @@ namespace SwaggerWebAPI.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        /// <summary>
+        /// Gets values from the ValuesController
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// <code>GET api/values</code>
+        /// </remarks>
+        /// <returns>A list of string values</returns>
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        /// <summary>
+        /// Gets an Item object
+        /// <remarks>
+        /// Sample request:
+        /// <code>GET api/values/5</code>
+        /// </remarks>
+        /// </summary>
+        /// <param name="id">An integer value</param>
+        /// <returns>An Item with the entered integer value</returns>
+        /// <response code="200">If the entered value is ok</response>
+        /// <response code="400">If the entered value is negative</response>
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<Item> Get(int id)
         {
-            return "value";
+            if (0 > id)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                var item = new Item { Id = id, Name = $"Item {id}" };
+                return item;
+            }            
         }
 
         // POST api/values
--- SwaggerWebAPI/Startup.cs
+++ SwaggerWebAPI/Startup.cs
@@ -10,6 +10,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.AspNetCore.StaticFiles;
+using System.Reflection;
+using System.IO;
 
 namespace SwaggerWebAPI
 {
@@ -26,6 +30,33 @@ namespace SwaggerWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "Swagger sample",
+                    Version = "v1",
+                    Description = "A simple application to show the usage of Swagger",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Peter De Jonghe",
+                        Email = "email@gmail.com",
+                        Url = "https://twitter.com/pdejonghe"
+                    },
+                    License = new License
+                    {
+                        Name = "Some license",
+                        Url = "https://example.com/license"
+                    }
+                });
+
+                //Set the comments path for the Swagger JSON and UI ...
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +73,24 @@ namespace SwaggerWebAPI
             }
 
             app.UseHttpsRedirection();
+
+            //Enable middleware to server generated Swagger as a JSON endpoint ...
+            app.UseSwagger();
+
+            //Enable middleware to server SwaggerUI (HTML, JS, CS, ...), specifying the Swagger JSON endpoint ...
+            app.UseSwaggerUI(options =>
+            {
+                //f using directories with IIS or a reverse proxy, set the Swagger endpoint to a relative path using the ./ prefix, eg: "./swagger/v1/swagger.json". 
+                //Using "/swagger/v1/swagger.json" instructs the app to look for the JSON file at the true root of the URL (plus the route prefix, if used). 
+                //For example, use "http://localhost:<port>/<route_prefix>/swagger/v1/swagger.json" instead of "http://localhost:<port>/<virtual_directory>/<route_prefix>/swagger/v1/swagger.json".
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger sample v1");
+
+                //To serve the Swagger UI at the app's root (https://localhost:port/), set the RoutePrefix
+                //property to an empty string ...
+                //options.RoutePrefix = string.Empty;
+                //The default Swagger UI can be found at https://localhost:port/swagger ...
+            });
+
             app.UseMvc();
         }
     }
